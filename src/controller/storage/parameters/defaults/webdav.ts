cdec326bed148e5622b0969082a98044fb38d046
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-04-19 18:05:41 +0800

    Update webdav.ts

--- src/controller/storage/parameters/defaults/webdav.ts
+++ src/controller/storage/parameters/defaults/webdav.ts
@@ -1,5 +1,26 @@
-import { WebdavParamsAdvanced, WebdavParamsStandard } from "../../../../type/rclone/storage/parameters/webdav";
-import { DefaultParams } from "../../../../type/rclone/storage/defaults";
+
+import { DefaultParams, ParamsSelectType } from "../../../../type/rclone/storage/defaults";
+
+interface WebdavParamsStandard {
+    // 标准选项
+    url: string;
+    vendor?: ParamsSelectType;
+    user?: string;
+    pass?: string;
+    bearer_token?: string;
+}
+
+
+interface WebdavParamsAdvanced {
+    // 高级选项
+    bearer_token_command?: string;
+    encoding?: string;
+    headers?: Array<{ key: string, value: string }>; // 也可以使用 Map<string, string> 类型
+    pacer_min_sleep?: string; // 使用 Duration 类型，但此处为了简化表示，我们暂时将其作为字符串
+    nextcloud_chunk_size?: string; // 使用 SizeSuffix 类型，此处同样简化为字符串
+    owncloud_exclude_shares?: boolean;
+    description?: string;
+}
 
 const standard: WebdavParamsStandard = {
     url: "",
