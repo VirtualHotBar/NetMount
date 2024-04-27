50ff1d7a013fc44743bded3ce4825af1bd80bf96
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-04-27 17:24:46 +0800

    Delete src directory

--- src/type/hook.d.ts
+++
@@ -1,9 +0,0 @@
-interface Hooks{
-    upStats:Function;
-    upStorage:Function;
-    upMount:Function;
-    navigate:(path:string)=>void;
-    setLocaleStr:(localeStr:string)=>void|string;
-}
-
-export {Hooks}
--- src/type/page/storage/explorer.d.ts
+++
@@ -1,8 +0,0 @@
-interface clipListItem{
-    isMove:boolean;
-    storageName:string;
-    path:string;
-    isDir:boolean
-}
-
-export {clipListItem}
