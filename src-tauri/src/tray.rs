434c6734f218716991f8430b3ac0100c6d08b820
Flysoft <flysoftbeta@outlook.com>
2024-06-02 18:51:22 +0800

    system tray fix

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -17,37 +17,36 @@ impl Tray {
                 ])
                 .build()
                 .unwrap();
-            let tray = tauri::tray::TrayIconBuilder::new()
-                .menu(&menu)
-                .on_tray_icon_event(|icon, event| match event {
-                    tauri::tray::TrayIconEvent::Click {
-                        id: _,
-                        position: _,
-                        rect: _,
-                        button,
-                        button_state,
-                    } => {
-                        if button == tauri::tray::MouseButton::Left
-                            && button_state == tauri::tray::MouseButtonState::Up
-                        {
-                            icon.app_handle()
-                                .app_main_window()
-                                .toggle_visibility(None)
-                                .ok();
-                        }
+            let tray = app.tray_by_id("main").unwrap();
+            tray.set_menu(Some(menu))?;
+            tray.on_tray_icon_event(|icon, event| match event {
+                tauri::tray::TrayIconEvent::Click {
+                    id: _,
+                    position: _,
+                    rect: _,
+                    button,
+                    button_state,
+                } => {
+                    if button == tauri::tray::MouseButton::Left
+                        && button_state == tauri::tray::MouseButtonState::Up
+                    {
+                        icon.app_handle()
+                            .app_main_window()
+                            .toggle_visibility(None)
+                            .ok();
                     }
-                    _ => {}
-                })
-                .on_menu_event(|app, event| match event.id.as_ref() {
-                    "show" => {
-                        app.app_main_window().toggle_visibility(Some(true)).ok();
-                    }
-                    "quit" => {
-                        app.app_quit();
-                    }
-                    _ => {}
-                })
-                .build(app)?;
+                }
+                _ => {}
+            });
+            tray.on_menu_event(|app, event| match event.id.as_ref() {
+                "show" => {
+                    app.app_main_window().toggle_visibility(Some(true)).ok();
+                }
+                "quit" => {
+                    app.app_quit();
+                }
+                _ => {}
+            });
             Ok(Self(tray))
         })
     }
